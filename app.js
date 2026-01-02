d6ba52fdf4059fb2fbcca60299c79c6f221bf8e2
Dibya Ranjan Mohanta <dibyaranjan.mohanta59@gmail.com>
2026-01-02 18:33:12 +0530

    cors updated

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 const express = require('express')
-const path = require('path')
+const cors = require('cors')
+
 const app = express()
 const PORT = process.env.PORT || 4000
-const server = app.listen(PORT, () => console.log(`Running server on port ${PORT}`))
 
-const io = require('socket.io')(server)
+app.use(cors({
+    origin: "*"
+}))
 
-app.use(express.static(path.join(__dirname, '../frontend')))
+const server = app.listen(PORT, () =>
+    console.log(`🚀 Server running on port ${PORT}`)
+)
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
 
 let socketsConnected = new Set()
 
-io.on('connection', onConnected)
-
-function onConnected(socket) { 
-    console.log(`🟢 Socket connected successfully. Socket ID: ${socket.id}`);
+io.on('connection', (socket) => {
+    console.log(`🟢 Client connected | Socket ID: ${socket.id}`)
 
     socketsConnected.add(socket.id)
-
     io.emit('clients-total', socketsConnected.size)
 
-    socket.on('disconnect', () =>{
-        console.log('🔴 Socket Disconnected. Socket ID:', socket.id)
+    socket.on('disconnect', () => {
+        console.log(`🔴 Client disconnected | Socket ID: ${socket.id}`)
         socketsConnected.delete(socket.id)
         io.emit('clients-total', socketsConnected.size)
     })
 
-    socket.on('message',(data) => {
-        //console.log(data)
-        socket.broadcast.emit('chat-message',data)
+    socket.on('message', (data) => {
+        socket.broadcast.emit('chat-message', data)
     })
 
     socket.on('feedback', (data) => {
-        socket.broadcast.emit('feedback',data)
+        socket.broadcast.emit('feedback', data)
     })
-}
+})
